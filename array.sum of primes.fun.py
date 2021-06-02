f3d4e104913788419a08f0bf8c0e8c8766c7bfc8
arigishiva <84182333+arigishiva@users.noreply.github.com>
2021-06-02 16:39:35 +0530

    Add files via upload

---
+++ array.sum of primes.fun.py
@@ -0,0 +1,24 @@
+import math as m
+def isprime(num):
+    if num==1:
+        return 0
+    
+    s=int(m.sqrt(num))
+    for i in range(2,s+1):
+        if num%i==0:
+            return 0
+    return 1
+def findprime(n,data):
+     prime=[]
+     nonprime=[]
+     for i in data:
+         if isprime(i):
+            prime.append(i)
+         else:
+            nonprime.append(i)
+     return sum(prime),sum(nonprime)
+n=int(input())
+data=list(map(int,input().split()))
+prime,np=findprime(n,data)
+print(prime)
+print(np)
